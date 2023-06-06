a58745f39573d6a6992b80d7d7ed5e95c8b50fbe
wealthiduwe <wealthiduwe@gmail.com>
2023-06-06 17:07:57 -0400

    update: Updated Profile service and modal s

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,25 +1,49 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnChanges} from '@angular/core';
 import {AuthService} from "../auth.service";
 import {DatabaseService} from "../shared/services/database.service";
 import * as AuthActions from "../core/store/Auth/actions";
-import {AppStateInterface} from "../shared/interface/userAuth";
-import {Store} from "@ngrx/store";
+import {AppStateInterface, IProfile, IUser} from "../shared/interface/userAuth";
+import {select, Store} from "@ngrx/store";
+import {Router} from "@angular/router";
+import {errorSelector, isAuthenticated, isLoadingSelector} from "../core/store/Auth/selectors";
+import { Observable} from "rxjs";
+import {SimpleChanges} from "@angular/core";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-    constructor(public authService: AuthService, public store:Store<AppStateInterface>) {
+export class HeaderComponent implements OnInit, OnChanges {
+
+   isAuthenticated;
+
+
+
+    constructor(public authService: AuthService, private router:Router, public store:Store<AppStateInterface>) {
+
+      this.isAuthenticated = this.store.pipe(select(isAuthenticated))
+
+
+
     }
 
-    ngOnInit(): void {
+    ngOnChanges(changes: SimpleChanges) {
+      console.log('xxx')
     }
+
+  ngOnInit(): void {
+    }
+
+
 
     logout() {
 
       this.store.dispatch(AuthActions.logout())
+      this.router.navigate(["login"])
+
         // this.authService.logout();
     }
+
+
 }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -4,6 +4,10 @@ import {DatabaseInterface} from "../shared/interface/database-interface";
 import {faAt} from '@fortawesome/free-solid-svg-icons';
 import {ModalService} from "../shared/services/modal.service";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {select, Store} from "@ngrx/store";
+import {AppStateInterface, IProfile} from "../shared/interface/userAuth";
+import {currentUserSelector} from "../core/store/Profile/selectors";
+import {Observable} from "rxjs";
 
 @Component({
     selector: 'app-profile',
@@ -11,33 +15,38 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
     styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-    data: DatabaseInterface;
+    data: IProfile| null;
     faAt = faAt;
     profileForm: FormGroup;
     email: FormControl;
     // password: FormControl;
     fullName: FormControl;
-    phone: FormControl;
+    // phone: FormControl;
     gender: FormControl;
+   currentUser$: Observable<IProfile | null>;
 
-    constructor(private webStorage: WebStorageService, private modalService: ModalService) {
+    constructor(private store: Store<AppStateInterface>, private modalService: ModalService) {
+      this.currentUser$ = this.store.pipe(select(currentUserSelector))
+
+
+
     }
 
     ngOnInit(): void {
-        this.data = this.webStorage.retrieveFromStorage("currentUser");
-        this.fullName = new FormControl({value: this.data.fullName, disabled: true}, Validators.required);
+this.currentUser$.subscribe(x => this.data = x)
+        this.fullName = new FormControl({value: this.data?.name, disabled: true}, Validators.required);
         // this.email = new FormControl('', Validators.required);
         // this.DOB = new FormControl('', Validators.required);
-        this.email = new FormControl({value: this.data.email, disabled: true}, [Validators.required, Validators.email]);
-        this.gender = new FormControl(this.data.gender, Validators.required);
-        this.phone = new FormControl(this.data.phone, Validators.required);
+        this.email = new FormControl({value: this.data?.email, disabled: true}, [Validators.required, Validators.email]);
+        // this.gender = new FormControl(this.data.gender, Validators.required);
+        // this.phone = new FormControl(this.data.phone, Validators.required);
         // this.password = new FormControl('', [Validators.required, Validators.minLength(6)]);
         this.profileForm = new FormGroup({
             fullName: this.fullName,
             // DOB: this.DOB,
             email: this.email,
-            gender: this.gender,
-            phone: this.phone,
+            // gender: this.gender,
+            // phone: this.phone,
         });
     }
 
