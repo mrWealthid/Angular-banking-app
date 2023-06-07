3994e5e399263d3f0cd79dc03baf2272067f98fe
wealthiduwe <wealthiduwe@gmail.com>
2023-06-07 12:23:29 -0400

    add: Added Image upload feature for users

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,13 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {WebStorageService} from "../shared/services/web-storage.service";
 import {DatabaseInterface} from "../shared/interface/database-interface";
-import {faAt} from '@fortawesome/free-solid-svg-icons';
+import {faAt, faLock, faLockOpen, faUser, faUserEdit} from '@fortawesome/free-solid-svg-icons';
 import {ModalService} from "../shared/services/modal.service";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {select, Store} from "@ngrx/store";
 import {AppStateInterface, IProfile} from "../shared/interface/userAuth";
 import {currentUserSelector} from "../core/store/Profile/selectors";
-import {Observable} from "rxjs";
+import {BehaviorSubject, Observable} from "rxjs";
 
 @Component({
     selector: 'app-profile',
@@ -25,6 +25,14 @@ export class ProfileComponent implements OnInit {
     gender: FormControl;
    currentUser$: Observable<IProfile | null>;
 
+   currentPassword:FormControl
+  newPassword:FormControl
+  confirmPassword:FormControl
+
+   passwordForm:FormGroup
+
+   step = new BehaviorSubject(0)
+
     constructor(private store: Store<AppStateInterface>, private modalService: ModalService) {
       this.currentUser$ = this.store.pipe(select(currentUserSelector))
 
@@ -33,6 +41,7 @@ export class ProfileComponent implements OnInit {
     }
 
     ngOnInit(): void {
+     this.updatePasswordForm()
 this.currentUser$.subscribe(x => this.data = x)
         this.fullName = new FormControl({value: this.data?.name, disabled: true}, Validators.required);
         // this.email = new FormControl('', Validators.required);
@@ -50,7 +59,49 @@ this.currentUser$.subscribe(x => this.data = x)
         });
     }
 
+
+    updatePasswordForm() {
+      this.currentPassword = new FormControl('', [Validators.required, Validators.minLength(8)]);
+      // this.email = new FormControl('', Validators.required);
+      // this.DOB = new FormControl('', Validators.required);
+      this.newPassword = new FormControl('', [Validators.required, Validators.minLength(8)]);
+      this.confirmPassword = new FormControl('', [Validators.required, Validators.minLength(8)]);
+ this.passwordForm = new FormGroup({
+   passwordCurrent: this.currentPassword,
+   password: this.newPassword,
+   passwordConfirm:this.confirmPassword
+ })
+
+    }
+
     toggleModal() {
         this.modalService.HandleShowModal();
     }
+
+
+    handleFormUpdate(val:any) {
+
+     console.log('hi ')
+      this.step.next(1)
+    }
+
+  previewUrl: string | ArrayBuffer | null;
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.previewUrl = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  protected readonly faUser = faUser;
+  protected readonly faLock = faLock;
+  protected readonly faLockOpen = faLockOpen;
+
+  protected readonly faUserEdit = faUserEdit;
 }
