abfa9b52a1ee14600c07dd81c4cf6bfcd7a2d7f0
wealthiduwe <wealthiduwe@gmail.com>
2023-06-12 22:37:02 -0400

    Add: Created a reusable select component

--- src/app/shared/inputs/select-input/select-input.component.ts
+++ src/app/shared/inputs/select-input/select-input.component.ts
@@ -1,23 +1,45 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, forwardRef, Input} from '@angular/core';
+import {FormControl, NG_VALUE_ACCESSOR} from "@angular/forms";
 
 
 @Component({
   selector: 'select-input',
   templateUrl: './select-input.component.html',
-  styleUrls: ['./select-input.component.css']
+  styleUrls: ['./select-input.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectInputComponent),
+      multi: true
+    }
+  ]
 })
 export class SelectInputComponent {
   @Input() items: any[];
+  @Input() label: string
   @Input() multiple: boolean;
   @Input() bindLabel: string;
-  @Input() label: string;
   @Input() bindValue: string;
   @Input() placeholder: string;
-  @Output() selectionChange: EventEmitter<any[]> = new EventEmitter<any[]>();
+  @Input() formControl: FormControl
 
-  selectedItems: any[];
+  value: any;
+  onChange: any = () => {
+  };
+  onTouched: any = () => {
+  };
 
-  onSelectionChange(): void {
-    this.selectionChange.emit(this.selectedItems);
+  writeValue(value: any): void {
+    this.value = value;
   }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  
 }
