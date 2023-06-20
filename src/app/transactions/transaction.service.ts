d9fc203b4e2e069447134776974ce2c72d28eb00
wealthiduwe <wealthiduwe@gmail.com>
2023-06-20 13:07:38 -0400

    Add:Implemented search filters using http params

--- src/app/transactions/transaction.service.ts
+++ src/app/transactions/transaction.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {map, Observable} from "rxjs";
 import {IList} from "../shared/table/model/table-interface";
 
@@ -12,11 +12,36 @@ export class TransactionService {
 
   }
 
-  getListData(): Observable<IList> {
-    return this.Http.get<IList>('http://localhost:3000/api/v1/transactions').pipe(map((data: any) => {
+
+  createParams(params: any) {
+    let queryParams = new HttpParams();
+    let newObj = {
+      ...params.search,
+      limit: params.limit,
+      page: params.pageNumber + 1,
+      sort: '-createdAt'
+    }
+    console.log(newObj)
+    return queryParams.appendAll(newObj)
+    // queryParams = queryParams.append('limit', params.size);
+    // queryParams = queryParams.append('page', params.pageNumber + 1);
+
+
+  }
+
+  getListData(params?: any): Observable<IList> {
+
+    const query = this.createParams(params)
+    let queryParams = new HttpParams();
+    console.log(params)
+
+    // queryParams = queryParams.appendAll({'limit': params.size, 'page': params.pageNumber + 1});
+    // queryParams = queryParams.append('page', params.pageNumber + 1);
+    console.log(queryParams)
+    return this.Http.get<IList>(`http://localhost:3000/api/v1/transactions`, {params: query}).pipe(map((data: any) => {
       return {
         status: data.status,
-        length: data.length,
+        totalRecords: data.totalRecords,
         data: data.data
       }
 
