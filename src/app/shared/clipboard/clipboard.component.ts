9caff67cbf43cd63b0a6797e3484cc4d3fb5ecca
wealthiduwe <wealthiduwe@gmail.com>
2023-06-21 23:15:01 -0400

    Add:Added a new reusable component for clipboard copier

---
+++ src/app/shared/clipboard/clipboard.component.ts
@@ -0,0 +1,45 @@
+import {Component, Input} from '@angular/core';
+import {faCheck, faClipboard} from "@fortawesome/free-solid-svg-icons";
+import {BehaviorSubject} from "rxjs";
+
+@Component({
+  selector: 'app-clipboard',
+  template: `
+
+    <span (click)="copyToClipboard()">
+
+     <ng-container *ngIf="(valueState| async)"> <i class="fa font-thin cursor-pointer fa-check text-green-400 "></i>
+      </ng-container>
+
+    <ng-container *ngIf="!(valueState| async)"> <i class="fa text-green-400 fa-clipboard"></i>
+
+      </ng-container>
+    </span>
+    <!--    <button ">{{ buttonText }}</button>-->
+  `,
+})
+export class ClipboardComponent {
+  @Input() textToCopy: string;
+  @Input() buttonText: string;
+  timer: any
+  valueState = new BehaviorSubject(false)
+  protected readonly faClipboard = faClipboard;
+  protected readonly faCheck = faCheck;
+
+  copyToClipboard() {
+    clearTimeout(this.timer)
+    this.valueState.next(true)
+    navigator.clipboard.writeText(this.textToCopy)
+      .then(() => {
+
+        this.timer = setTimeout(() => {
+          this.valueState.next(false)
+        }, 2000);
+        console.log('Text copied to clipboard');
+      })
+      .catch((error) => {
+        console.error('Error copying text to clipboard:', error);
+      });
+  }
+
+}
--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -1,41 +1,28 @@
 import {Component, OnInit} from '@angular/core';
 import {TransactionService} from "./transaction.service";
+import {faClipboard} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
   styleUrls: ['./transactions.component.css'],
 
-
-  // providers: [{provide: BsDropdownConfig, useValue: {isAnimated: true, autoClose: true}}]
 })
 export class TransactionsComponent implements OnInit {
 
-
-  testData = [
-    {id: 1, name: 'John Doe', age: 25, city: 'New York'},
-    {id: 2, name: 'Jane Smith', age: 32, city: 'Los Angeles'},
-    {id: 3, name: 'Michael Johnson', age: 40, city: 'Chicago'},
-    {id: 4, name: 'Emily Davis', age: 28, city: 'San Francisco'},
-    {id: 5, name: 'Robert Wilson', age: 35, city: 'Houston'},
-    {id: 6, name: 'Sophia Brown', age: 30, city: 'Seattle'},
-    {id: 7, name: 'William Lee', age: 45, city: 'Miami'},
-    {id: 8, name: 'Olivia Harris', age: 27, city: 'Boston'},
-    {id: 9, name: 'Daniel Clark', age: 38, city: 'Dallas'},
-    {id: 10, name: 'Mia Lewis', age: 33, city: 'Denver'}
-  ];
+  // faCopy = faCopy;
+  faClipboard = faClipboard;
   tableColumns = [
     {prop: 'id', name: 'Id'},
     {prop: 'depositorName', name: 'Payer'},
-    {prop: 'amount', name: 'Amount', pipe: "Currency"},
+    {prop: 'amount', name: 'Amount', pipe: "Currency", custom: true},
     {prop: 'transactionType', name: 'Transaction', custom: true},
     {prop: 'depositorAccountNumber', name: 'Payer Account', custom: true},
     {prop: 'createdAt', name: 'Time', pipe: "Date"}
   ];
-  currentPage = 0;
-  itemsPerPage = 5;
-  totalItems = this.testData.length;
+
   protected readonly TransactionService = TransactionService;
+
 
   constructor(public service: TransactionService) {
   }
