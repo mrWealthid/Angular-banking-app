de57570e2294e35f517b2f73fa62caed201823ee
wealthiduwe <wealthiduwe@gmail.com>
2023-06-23 17:48:19 -0400

    Update: Updates on dashboard route definition

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,25 +1,23 @@
 import {Component, OnInit} from '@angular/core';
-import {DatabaseInterface} from "../shared/interface/database-interface";
-import {WebStorageService} from "../shared/services/web-storage.service";
 
 @Component({
-    selector: 'app-dashboard',
-    templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.css']
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
   links = [
     {path: '/dashboard', text: 'Overview'},
-    {path: 'profile', text: 'User'},
-    {path: 'transactions', text: 'Transactions'}
+    {path: 'profile', text: 'Profile'},
+    {path: 'transactions', text: 'Transactions'}, {path: 'payments', text: 'Payments'}
   ]
-    constructor() {
-    }
+
+  constructor() {
+  }
 
 
+  ngOnInit(): void {
 
-    ngOnInit(): void {
-
-    }
+  }
 }
