d9589ccf03ff9bbbf806bde942a4d23357e0bf88
wealthiduwe <wealthiduwe@gmail.com>
2023-06-26 17:03:06 -0400

    add: Added user photo avatar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,49 +1,47 @@
-import {Component, OnInit, OnChanges} from '@angular/core';
+import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {AuthService} from "../auth.service";
-import {DatabaseService} from "../shared/services/database.service";
 import * as AuthActions from "../core/store/Auth/actions";
-import {AppStateInterface, IProfile, IUser} from "../shared/interface/userAuth";
+import {AppStateInterface, IProfile} from "../shared/interface/userAuth";
 import {select, Store} from "@ngrx/store";
 import {Router} from "@angular/router";
-import {errorSelector, isAuthenticated, isLoadingSelector} from "../core/store/Auth/selectors";
-import { Observable} from "rxjs";
-import {SimpleChanges} from "@angular/core";
+import {isAuthenticated} from "../core/store/Auth/selectors";
+import {Observable} from "rxjs";
+import {currentUserSelector} from "../core/store/Profile/selectors";
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss']
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit, OnChanges {
 
-   isAuthenticated;
+  isAuthenticated;
+  private currentUser: Observable<IProfile | null>;
 
 
+  constructor(public authService: AuthService, private router: Router, public store: Store<AppStateInterface>) {
 
-    constructor(public authService: AuthService, private router:Router, public store:Store<AppStateInterface>) {
-
-      this.isAuthenticated = this.store.pipe(select(isAuthenticated))
-
+    this.isAuthenticated = this.store.pipe(select(isAuthenticated))
+    this.currentUser = this.store.pipe(select(currentUserSelector))
 
 
-    }
+  }
 
-    ngOnChanges(changes: SimpleChanges) {
-      console.log('xxx')
-    }
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('xxx')
+  }
 
   ngOnInit(): void {
-    }
+  }
 
 
+  logout() {
 
-    logout() {
+    this.store.dispatch(AuthActions.logout())
+    this.router.navigate(["login"])
 
-      this.store.dispatch(AuthActions.logout())
-      this.router.navigate(["login"])
-
-        // this.authService.logout();
-    }
+    // this.authService.logout();
+  }
 
 
 }
