a72fb5a323783ed7740468ee483c02941e469d28
wealthiduwe <wealthiduwe@gmail.com>
2023-07-05 01:30:25 -0400

    add: created a new model for dashboard stats and integrated chart with my backend

---
+++ src/app/dashboard/dashboard.model.ts
@@ -0,0 +1,14 @@
+export interface IDashboardStats {
+  x: String
+  y: Number
+
+}
+
+export interface IDashboardData {
+  name?: string,
+  data: IDashboardStats[]
+
+
+}
+
+
--- src/app/dashboard/overview/overview.component.ts
+++ src/app/dashboard/overview/overview.component.ts
@@ -1,29 +1,44 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DashboardService} from "../dashboard.service";
+import {IDashboardData} from "../dashboard.model";
+import {forkJoin} from "rxjs";
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.css']
 })
-export class OverviewComponent {
+export class OverviewComponent implements OnInit {
 
-  chartOptions = {
-    series: [
-      {
-        name: "My-series",
-        data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
-      }
-    ],
-    chart: {
-      height: 350,
-      type: "bar"
-    },
-    title: {
-      text: "My First Angular Chart"
-    },
-    xaxis: {
-      categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"]
-    }
-  };
+  // chartOptions = {
+  //   series: [
+  //     {
+  //       name: "My-series",
+  //       data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+  //     }
+  //   ],
+  //   chart: {
+  //     height: 350,
+  //     type: "bar"
+  //   },
+  //   title: {
+  //     text: "My First Angular Chart"
+  //   },
+  //   xaxis: {
+  //     categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"]
+  //   }
+  // };
+  series: IDashboardData[]
+
+  constructor(private dashboardService: DashboardService) {
+
+    forkJoin([this.dashboardService.getStatsData('Credit'), this.dashboardService.getStatsData('Debit')]).subscribe(data => {
+      this.series = (data.map(x => x[0]))
+    })
+  }
+
+  ngOnInit() {
+
+  }
 
 }
