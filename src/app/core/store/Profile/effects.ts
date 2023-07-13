19d9ffd6389eb8cac8cf179c327a8c8ffd81ab02
wealthiduwe <wealthiduwe@gmail.com>
2023-07-12 23:03:22 -0400

    update: updates

--- src/app/core/store/Profile/effects.ts
+++ src/app/core/store/Profile/effects.ts
@@ -7,15 +7,15 @@ import {IProfile} from "../../../shared/interface/userAuth";
 import {Router} from "@angular/router";
 import {ProfileService} from "../../../profile/profile.service";
 import {NotificationService} from "../../../shared/services/notification.service";
+import {AuthService} from "../../../auth.service";
 
 @Injectable()
 export class ProfileEffect {
+  private authService = inject(AuthService);
   private Notify = inject(NotificationService);
   private router = inject(Router);
   private profileService = inject(ProfileService);
   private actions$ = inject(Actions);
-
-
   profilePasswordUpdate$ = createEffect(() =>
     this.actions$.pipe(ofType(AuthActions.profilePasswordUpdate),
       switchMap((payload) => {
@@ -24,16 +24,6 @@ export class ProfileEffect {
         }))));
       }))
   );
-
-  profile$ = createEffect(() =>
-    this.actions$.pipe(ofType(AuthActions.profileLookup),
-      mergeMap(() => {
-        return this.profileService.fetchProfile().pipe(map((newUser: IProfile) => AuthActions.profileLookupSuccess({newUser})), tap(() => this.router.navigate(['dashboard'])), catchError(error => of(AuthActions.profileLookupFailure({
-          error: error.message
-        }))));
-      }))
-  );
-
   profileUpdate$ = createEffect(() =>
     this.actions$.pipe(ofType(AuthActions.profileUpdate),
       switchMap((payload) => {
@@ -47,6 +37,23 @@ export class ProfileEffect {
         }))));
       }))
   );
+  profile$ = createEffect(() =>
+    this.actions$.pipe(ofType(AuthActions.profileLookup),
+      mergeMap(() => {
+        return this.profileService.fetchProfile().pipe(map((newUser: IProfile) => AuthActions.profileLookupSuccess({newUser})), tap(() => {
+          this.checkForPrevRoute(this.authService.storedRoutes) ?
+            this.router.navigate([this.authService.storedRoutes[0]]) : this.router.navigate(['dashboard'])
 
+        }), catchError(error => of(AuthActions.profileLookupFailure({
+          error: error.message
+        }))));
+      }))
+  );
+
+
+  checkForPrevRoute(url: any[]) {
+    return url.length > 0;
+
+  }
 }
 
