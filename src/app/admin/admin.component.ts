878e8d276947003973954e6b804e371a4e9939ee
wealthiduwe <wealthiduwe@gmail.com>
2023-07-15 14:28:36 -0400

    add: clean up

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {DatabaseService} from "../shared/services/database.service";
-import {DatabaseInterface} from "../shared/interface/database-interface";
+
 
 @Component({
   selector: 'app-admin',
@@ -8,10 +7,9 @@ import {DatabaseInterface} from "../shared/interface/database-interface";
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-  dropDownList: number;
-  users: DatabaseInterface[];
+  
 
-  constructor(public db: DatabaseService) {
+  constructor() {
   }
 
   ngOnInit(): void {
