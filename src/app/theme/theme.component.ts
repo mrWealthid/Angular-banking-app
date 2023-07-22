50451336181c0e770663fe9b22945fc7c25359f7
wealthiduwe <wealthiduwe@gmail.com>
2023-07-22 17:24:16 -0400

    update: building themming toggler

---
+++ src/app/theme/theme.component.ts
@@ -0,0 +1,23 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-theme',
+  templateUrl: './theme.component.html',
+  styleUrls: ['./theme.component.css']
+})
+export class ThemeComponent {
+
+  isDarkMode = false;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  toggleTheme(): void {
+
+  }
+
+
+}
