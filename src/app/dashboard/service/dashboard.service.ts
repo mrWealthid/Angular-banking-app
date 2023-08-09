abb5643629c63cd14a617ce913db18cef322aaf1
wealthiduwe <wealthiduwe@gmail.com>
2023-08-08 23:16:48 -0400

    add: added pending state, to loan chart

--- src/app/dashboard/service/dashboard.service.ts
+++ src/app/dashboard/service/dashboard.service.ts
@@ -1,8 +1,8 @@
-import {inject, Injectable} from '@angular/core';
-import {map, Observable} from "rxjs";
-import {HttpClient} from "@angular/common/http";
-import {IDashboardData, IDashboardStats, ISummary} from "../dashboard.model";
-import {environment} from "../../../environments/environment";
+import { inject, Injectable } from '@angular/core';
+import { map, Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { IDashboardData, IDashboardStats, ISummary } from "../dashboard.model";
+import { environment } from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +13,7 @@ export class DashboardService {
 
 
   getMonthlyStatsData(type: string, year: number = new Date(Date.now()).getFullYear()): Observable<IDashboardData[]> {
-    return this.Http.get(`${environment.API_URL}/api/v1/transactions/monthly-stats/${type}/${year}`).pipe(map(({data}: any) => {
+    return this.Http.get(`${environment.API_URL}/api/v1/transactions/monthly-stats/${type}/${year}`).pipe(map(({ data }: any) => {
       return ([
         {
           name: type,
@@ -23,20 +23,20 @@ export class DashboardService {
     }));
   }
   getMonthlyUserStatsData(type: string, year: number = new Date(Date.now()).getFullYear()): Observable<IDashboardData[]> {
-    return this.Http.get(`${environment.API_URL}/api/v1/users/monthly-stats/${type}/${year}`).pipe(map(({data}: any) => {
+    return this.Http.get(`${environment.API_URL}/api/v1/users/monthly-stats/${type}/${year}`).pipe(map(({ data }: any) => {
       return ([
         {
-          name: type,
+          name: type[0].toUpperCase() + type.slice(1).toLowerCase(),
           data: this.transformUserChartData(data.stats)
         }
       ])
     }));
   }
   getMonthlyLoanStatsData(type: string, year: number = new Date(Date.now()).getFullYear()): Observable<IDashboardData[]> {
-    return this.Http.get(`${environment.API_URL}/api/v1/loans/monthly-stats/${type}/${year}`).pipe(map(({data}: any) => {
+    return this.Http.get(`${environment.API_URL}/api/v1/loans/monthly-stats/${type}/${year}`).pipe(map(({ data }: any) => {
       return ([
         {
-          name: type,
+          name: type[0] + type.slice(1).toLowerCase(),
           data: this.transformUserChartData(data.stats)
         }
       ])
@@ -44,28 +44,28 @@ export class DashboardService {
   }
 
 
-  getLoanStats () {
-      return this.Http.get(`${environment.API_URL}/api/v1/loans/loan-stats`).pipe(map(({data}:any)=> {
-        return {
-          approved: data[0]?.APPROVED?.totalCount || 0,
-          declined: data[0]?.DECLINED?.totalCount || 0,
-          pending: data[0]?.PENDING?.totalCount || 0
-        }
-      }))
-    
+  getLoanStats() {
+    return this.Http.get(`${environment.API_URL}/api/v1/loans/loan-stats`).pipe(map(({ data }: any) => {
+      return {
+        approved: data[0]?.APPROVED?.totalCount || 0,
+        declined: data[0]?.DECLINED?.totalCount || 0,
+        pending: data[0]?.PENDING?.totalCount || 0
+      }
+    }))
+
   }
-  getUserStats () {
-      return this.Http.get(`${environment.API_URL}/api/v1/users/user-stats`).pipe(map(({data}:any)=> {
-        return {
-          user: data[0]?.user?.totalCount || 0,
-          admin: data[0]?.admin?.totalCount || 0,
-        }
-      }))
-    
+  getUserStats() {
+    return this.Http.get(`${environment.API_URL}/api/v1/users/user-stats`).pipe(map(({ data }: any) => {
+      return {
+        user: data[0]?.user?.totalCount || 0,
+        admin: data[0]?.admin?.totalCount || 0,
+      }
+    }))
+
   }
 
   getStatsData(type: string = 'month', time: number = new Date().getMonth()): Observable<ISummary> {
-    return this.Http.get(`${environment.API_URL}/api/v1/transactions/stats/${type}/${time}`).pipe(map(({data}: any) => {
+    return this.Http.get(`${environment.API_URL}/api/v1/transactions/stats/${type}/${time}`).pipe(map(({ data }: any) => {
 
       if (data.stats.length === 0) {
         return {
@@ -74,7 +74,7 @@ export class DashboardService {
           profit: 0,
         }
       }
-      const {Credit = {totalAmount: 0}, Debit = {totalAmount: 0}} = data.stats[0]
+      const { Credit = { totalAmount: 0 }, Debit = { totalAmount: 0 } } = data.stats[0]
       return (
         {
           totalCredit: Credit.totalAmount,
@@ -87,28 +87,28 @@ export class DashboardService {
 
 
 
-  getMonthlyHighlight () {
-    return this.Http.get(`${environment.API_URL}/api/v1/transactions/monthly-highlights`).pipe(map(({data}:any)=> {
-     
+  getMonthlyHighlight() {
+    return this.Http.get(`${environment.API_URL}/api/v1/transactions/monthly-highlights`).pipe(map(({ data }: any) => {
+
       return {
         credit: data[0]?.Credit?.totalAmount || 0,
         debit: Math.abs(data[0]?.Debit?.totalAmount) || 0,
-      
+
       }
     }))
-  
-}
+
+  }
 
 
-getDailyHighlight () {
-  return this.Http.get(`${environment.API_URL}/api/v1/transactions/daily-highlights`).pipe(map(({data}:any)=> {
-    return {
-      credit: data[0]?.Credit?.totalAmount || 0,
-      debit: Math.abs(data[0]?.Debit?.totalAmount) || 0,
-    }
-  }))
+  getDailyHighlight() {
+    return this.Http.get(`${environment.API_URL}/api/v1/transactions/daily-highlights`).pipe(map(({ data }: any) => {
+      return {
+        credit: data[0]?.Credit?.totalAmount || 0,
+        debit: Math.abs(data[0]?.Debit?.totalAmount) || 0,
+      }
+    }))
 
-}
+  }
 
 
 
@@ -121,7 +121,7 @@ getDailyHighlight () {
     }
     return response
   }
-transformUserChartData(stats: any[]): IDashboardStats[] {
+  transformUserChartData(stats: any[]): IDashboardStats[] {
     let response: IDashboardStats[] = [];
     for (const data of stats) {
       response.push({
