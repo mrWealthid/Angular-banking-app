ade9a2e5b903cd0a765bbde62846ca1e54fe929a
wealthiduwe <wealthiduwe@gmail.com>
2023-08-11 08:50:44 -0400

    fix: fix scroll to top position

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {HomeComponent} from './home/home.component';
-import {RouterModule} from "@angular/router";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {HeaderComponent} from './header/header.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AdminComponent} from './admin/admin.component';
-import {StoreDevtoolsModule} from "@ngrx/store-devtools";
-import {EffectsModule} from "@ngrx/effects";
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdminComponent } from './admin/admin.component';
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { EffectsModule } from "@ngrx/effects";
 
-import {StoreModule} from "@ngrx/store";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {effects, localStorageSyncReducer, reducers} from "./core/store";
-import {HeadersInterceptor} from "./headers.interceptor";
-import {CurrencyPipe} from "@angular/common";
-import {routes} from "./app-routes";
-import {SharedModule} from  "../app/shared/shared.module"
-import {ThemeComponent} from './theme/theme.component';
-import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { StoreModule } from "@ngrx/store";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { effects, localStorageSyncReducer, reducers } from "./core/store";
+import { HeadersInterceptor } from "./headers.interceptor";
+import { CurrencyPipe } from "@angular/common";
+import { routes } from "./app-routes";
+import { SharedModule } from "../app/shared/shared.module"
+import { ThemeComponent } from './theme/theme.component';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { UserLoanComponent } from './users/user-loan/user-loan.component';
 
 
@@ -32,7 +32,7 @@ import { UserLoanComponent } from './users/user-loan/user-loan.component';
     AdminComponent,
     ThemeComponent,
     UserLoanComponent,
-   
+
 
 
   ],
@@ -56,13 +56,13 @@ import { UserLoanComponent } from './users/user-loan/user-loan.component';
         persist: true
       }
     }),
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { scrollPositionRestoration: "top" }),
     BrowserAnimationsModule,
     SharedModule,
     FormsModule,
     FontAwesomeModule
   ],
-  providers: [{provide: HTTP_INTERCEPTORS, useClass: HeadersInterceptor, multi: true}, CurrencyPipe],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: HeadersInterceptor, multi: true }, CurrencyPipe],
   bootstrap: [AppComponent]
 })
 
