c3df1979d39de32e0d789b4c0bf429f994486fff
wealthiduwe <wealthiduwe@gmail.com>
2023-08-11 08:50:24 -0400

    Update: Updates to functional role guard

--- src/app/shared/services/guards/role.guard.ts
+++ src/app/shared/services/guards/role.guard.ts
@@ -1,31 +1,32 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from "@angular/core";
 import { select, Store } from "@ngrx/store";
-import { AppStateInterface } from "../../interface/userAuth";
+import { AppStateInterface, IProfile } from "../../interface/userAuth";
 import { isAuthenticated } from "../../../core/store/Auth/selectors";
 import { currentUserSelector } from "../../../core/store/Profile/selectors";
 import { filter, forkJoin, map } from "rxjs";
 import { AuthService } from "../../../auth/auth.service";
 
-export const roleGuard: CanActivateFn = (route, state) => {
-  const isAuthenticated$ = inject(Store<AppStateInterface>).pipe(select(isAuthenticated))
-  const currentUser$ = inject(Store<AppStateInterface>).pipe(select(currentUserSelector))
-  const router = inject(Router)
-  const requiredRole = route.data['requiredRole'];
+export const roleGuard = (role: 'user' | 'admin'): CanActivateFn => {
 
-  return currentUser$
-    .pipe(filter((x) => x?.role !== undefined),
-      map((x) => {
+  const guard: CanActivateFn = (route, state) => {
+    const isAuthenticated$ = inject(Store<AppStateInterface>).pipe(select(isAuthenticated))
+    const currentUser$ = inject(Store<AppStateInterface>).pipe(select(currentUserSelector))
+    const router = inject(Router)
+    const requiredRole = role
+    return currentUser$
+      .pipe(filter((x) => x?.role !== undefined),
+        map((x: any) => {
+          if (!(requiredRole.includes(x?.role))) {
+            router.navigate(['dashboard/notAuthorized'])
+            return false;
+          }
 
-        if (!requiredRole.includes(x?.role)) {
-          router.navigate(['dashboard/notAuthorized'])
-          return false;
-        }
 
-
-        return true
-      }))
-
+          return true
+        }))
+  }
+  return guard
 };
 
 
